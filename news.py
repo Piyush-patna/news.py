9c544d0635715d8007a30b6b095201ff5564583c
Piyush-patna <piyushkumar9415@gmail.com>
2025-06-02 15:55:43 +0530

    Add files via upload

---
+++ news.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pickle
+import spacy
+
+with open('data.pkl', 'rb') as model_file:
+    model= pickle.load(model_file)
+
+nlp= spacy.load('en_core_web_lg')
+st.title("News Detection App")
+st.write("This app dtects whether news is real or fake")
+
+a= st.text_input('Enter News')
+
+doc= nlp(a)
+vector= doc.vector
+prediction= model.predict([vector])
+if prediction[0]== 1:
+    st.write('Fake news')
+else:
+    st.write('Real News')
